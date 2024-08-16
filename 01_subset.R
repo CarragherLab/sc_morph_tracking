eafac8e3decb6374ab24dde6f46ea3c27325edc1
beckaellen <29772508+beckaellen@users.noreply.github.com>
2024-08-16 09:24:43 +0100

    Tidied 01_subset.R

--- 01_subset.R
+++ 01_subset.R
@@ -64,6 +64,7 @@ timecourse_cleaned <- timecourse_cleaned %>%
 val_cleaned <- val_cleaned %>%
   select(all_of(common_cols))
 
+#remove unwanted columns from datasets
 filter = "^Image_|FileName|Object.?Number|Image.?Number|^URL|Location|[XY]$|Phase|NumberOfNeighbors|Orientation|Extent|BoundingBox|Moment|EulerNumber"
 removed_features = grep(filter, colnames(timecourse_cleaned), value = TRUE)
 
@@ -73,6 +74,9 @@ timecourse_cleaned<- timecourse_cleaned %>%
 
 val_cleaned<- val_cleaned %>%
   select(-all_of(removed_features))
+
+cleaned <- rbind(timecourse_cleaned, val_cleaned)
+fwrite(cleaned, "data/01_combined_cleaned.csv")
 
 
 #Median normalise the entire dataset using "standardise" from datawizard
@@ -87,49 +91,10 @@ val_scaled <- val_cleaned %>%
   na.omit()
 
 
-cleaned <- rbind(timecourse_cleaned, val_cleaned)
-fwrite(cleaned, "data/01_combined_cleaned.csv")
-
 rm(cleaned)
 rm(timecourse_cleaned)
 rm(val_cleaned)
 
 standardised <- rbind(timecourse_scaled, val_scaled)
 fwrite(standardised, "data/01_combined_standardised.csv")
-
-rm(list=c("timecourse_scaled", "val_scaled", "common_cols", "filter", "removed_features", "cleaned_ids"))
-
-
-#split meta and feature data
-f_dat <- standardised %>%
-  select(-starts_with("Metadata")) 
-m_dat <- standardised %>%
-  select(starts_with("Metadata"))
-
-
-### PCA on scaled dataset
-data_pca <-  prcomp(f_dat)
-
-
-p1 <- ggplot(as.data.frame(data_pca$x)) +
-  geom_point(aes(PC1, PC2, color=m_dat$Metadata_plate), alpha=0.3) +
-  labs(color="plate") 
-#scale_color_viridis_d(option="C")
-
-ggsave("../outputs/01_pca_by_plate.png", p1, height=6, width = 7)
-
-rm(p1)
-
-umap_df <- data.frame(m_dat, umap(data_pca$x[,1:10])$layout)
-
-rm(list=c("data_pca", "f_dat", "m_dat"))
-
-fwrite(umap_df, "../data/01_batchcorrected_subset_umap.csv")
-
-p4 <- ggplot(umap_df, aes(X1, X2))+ 
-  geom_point(aes(colour = Metadata_plate), alpha = 0.3)+
-  scale_color_viridis_d()+
-  labs(colour="")
-
-ggsave("../outputs/01_umap_by_plate.png",p4)
 
